7a7751eef3771d5a9a6ddf87ded7d38b522410cd
veeramradhika <veeramradhika@gmail.com>
2022-05-17 20:28:05 +0530

    program.cs (object) file add.cs (class) file

---
+++ claculator/Add.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorOper
+{
+    internal class Add
+    {
+       public int num1;
+       public int num2;
+       public int num3;
+        public void AddNumb()
+        {
+            num1 = 10;
+            num2 = 20;
+            num3 = num1 + num2;
+            Console.WriteLine("the addition of 10 and 20 is : " + num3);
+        }
+        public void sub()
+        {
+            num3 = num1 - num2;
+            Console.WriteLine("the substraction of 10 and 20 is : " + num3);
+        }
+
+    }
+}
---
+++ claculator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorOper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Add obj1 = new Add();
+            obj1.AddNumb();
+            obj1.sub();
+            Console.ReadLine();
+        }
+    }
+}
