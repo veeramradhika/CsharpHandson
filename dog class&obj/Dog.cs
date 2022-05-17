cb4b1fbccedccc242b7653afcc1f74bbd23cd441
veeramradhika <veeramradhika@gmail.com>
2022-05-17 19:58:27 +0530

    added program(dog object) file and Dog(class) file.

---
+++ dog class&obj/Dog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogClass
+{
+    internal class Dog
+    {
+        public string breed;
+        public void bark()
+        {
+            Console.WriteLine(breed + " bark like bow bow!!");
+        }
+    }
+}
---
+++ dog class&obj/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // create Dog object 
+            Dog dogobj = new Dog();
+            dogobj.breed = "Bull Dog";
+            Console.WriteLine(dogobj.breed);
+            dogobj.bark();
+            Console.ReadLine();
+        }
+    }
+}
