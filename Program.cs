b96359e42e13d704bff7050854d3f9c2297caea1
veeramradhika <veeramradhika@gmail.com>
2022-05-17 20:03:13 +0530

    added program(user object) file and users(class) file

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsObject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Users userobj = new Users(100,"John",1234);
+            userobj.GetUserDetails();
+            Console.WriteLine("successfully got the user details");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Users.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsObject
+{
+    internal class Users
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        private int password { get; set; }
+        public Users(int Id, string Name, int Password)
+        {
+            id = Id;
+            name = Name;
+            password = Password;
+        }
+        public void GetUserDetails()
+        {
+            Console.WriteLine("Id: {0}, Name: {1}, password: {2}", id, name, password);
+        }
+    }
+}
