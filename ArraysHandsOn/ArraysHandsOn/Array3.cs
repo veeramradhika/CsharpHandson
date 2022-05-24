24245d0a96d583e6e821708a913437b62990f6e8
veeramradhika <101562316+veeramradhika@users.noreply.github.com>
2022-05-24 19:49:32 +0530

    added ArraysHandsOn file

---
+++ ArraysHandsOn/ArraysHandsOn/Array3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ArraysHandsOn
+{
+    public class Array3
+    {
+        public void data()
+        {
+            bool[] a = new bool[10]
+            {
+                true,false,true,false,true,false,true,false,true,false
+            };
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+            Console.WriteLine("Alternate truefalse" );
+            Console.ReadLine();
+        }
+    }
+}
