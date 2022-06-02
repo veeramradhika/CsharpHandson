ee42b30a97de1bcbbb994827a2f62745c1cd7b55
veeramradhika <101562316+veeramradhika@users.noreply.github.com>
2022-06-02 17:27:49 +0530

    added file Reflection Hands on

---
+++ ReflectionHandsOn/ReflectionHandsOn/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionHandsOn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Reflection
+            //Intialize 't' as type of string
+            Type t = typeof(string);
+            //use reflection to find about any sort of data related to t
+            Console.WriteLine("name: {0}", t.Name);
+            Console.WriteLine("nameSpace: {0}", t.Namespace);
+            Console.WriteLine("FullName: {0}", t.FullName);
+            Console.WriteLine("bASETYPE: {0}", t.BaseType);
+            Console.ReadLine();
+
+            // Declare Instance of class Assembly
+            // Call the GetExecutingAssembly method
+            // to load the current assembly
+            Assembly executing = Assembly.GetExecutingAssembly();
+
+            // Array to store types of the assembly
+            Type[] types = executing.GetTypes();
+            foreach (var item in types)
+            {
+                //display each type
+                Console.WriteLine("class: {0}", item.Name);
+
+                MethodInfo[] methods = item.GetMethods();
+                foreach (var method in methods)
+                {
+                    Console.WriteLine("Methods: {0}", method.Name);
+
+                    // Array to store parameters
+                    ParameterInfo[] parameters = method.GetParameters();
+                    foreach (var arg in parameters)
+                    {
+                        Console.WriteLine("Parameters: {0} Type : {1}", arg.Name, arg.ParameterType);
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ReflectionHandsOn/ReflectionHandsOn/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionHandsOn
+{
+    class Student
+    {
+        //properities defination
+        public int RollNo { get; set; }
+        public string Name { get; set; }
+        //constructor with no argument
+        public Student()
+        {
+            RollNo = 0;
+            Name = String.Empty;
+        }
+        //constructor with parameters
+        public Student(int rollno, string name)
+        {
+            RollNo = rollno;
+            Name = name;
+        }
+        //method to display student data
+        public void DisplayData()
+        {
+            Console.WriteLine("Roll No: {0}", RollNo);
+            Console.WriteLine("Name: {0}", Name);
+
+        }
+    }
+}
