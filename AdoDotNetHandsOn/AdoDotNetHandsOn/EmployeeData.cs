d8e6fb0f4c2715fabf0053e8c31b600954ab8e9e
veeramradhika <101562316+veeramradhika@users.noreply.github.com>
2022-06-14 14:04:29 +0530

    Added ADO dot Net Hands On

---
+++ AdoDotNetHandsOn/AdoDotNetHandsOn/EmployeeData.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace AdoDotNetHandsOn
+{
+    public class EmployeeData
+    {
+        public static string sqlConnectionStr = "Data Source=RADHIKA;Initial Catalog = BankDb; Integrated Security = True";
+        public string insertEmployee()
+        {
+            Console.WriteLine("Enter the Emp Id: ");
+            int empId = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the Emp Name: ");
+            string empName = Console.ReadLine();
+
+            Console.WriteLine("Enter the Emp Mobile: ");
+            decimal empMobile = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Enter the Emp address: ");
+            string empAddress = Console.ReadLine();
+
+            //inserting the values into the sql
+            SqlConnection sqlConnection = new SqlConnection(sqlConnectionStr);
+            SqlCommand cmd = new SqlCommand("insert into Employee values("+empId+",'"+empName+"',"+empMobile+",'"+empAddress+"')",sqlConnection);
+            sqlConnection.Open();
+            cmd.ExecuteNonQuery();
+            sqlConnection.Close();
+            return "values are inserted";
+        }
+        public string UpdateEmployee()
+        {
+            Console.WriteLine("Enter the Emp Id to update: ");
+            int empId = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the Emp Name to update: ");
+            string empName = Console.ReadLine();
+
+            Console.WriteLine("Enter the Emp Mobile to update: ");
+            decimal empMobile = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Enter the Emp address to update: ");
+            string empAddress = Console.ReadLine();
+
+
+            //insert customer data into sqlserver
+            SqlConnection sqlConnection = new SqlConnection(sqlConnectionStr);//connection establishment
+            SqlCommand cmd = new SqlCommand("update Employee set EmpName='" + empName + "' , EmpMobile='" + empMobile + "' , EmpAddress='" + empAddress + "' where EmpId="+empId+ "", sqlConnection);
+            sqlConnection.Open();//connection state is open
+            int result = cmd.ExecuteNonQuery();//execute my sql commands 1
+            sqlConnection.Close(); //connection state is close
+            if (result == 0)
+                return "Not Updated";
+            return "Updated";
+        }
+        public string DeleteEmployee(int EmpId)
+        {
+            SqlConnection sqlConnection = new SqlConnection(sqlConnectionStr);//connection establishment
+            SqlCommand cmd = new SqlCommand("delete from Employee where EmpId=" + EmpId, sqlConnection);
+            sqlConnection.Open();//connection state is open
+            int result = cmd.ExecuteNonQuery();//execute my sql commands 1
+            sqlConnection.Close(); //connection state is close
+            if (result == 0)
+                return "Not Deleted";
+            return "Deleted";
+        }
+        public DataTable SelectEmployees()
+        {
+            SqlConnection sqlConnection = new SqlConnection(sqlConnectionStr);//connection establishment
+            string db = sqlConnection.Database;
+            SqlCommand cmd = new SqlCommand("select * from Employee", sqlConnection);
+            sqlConnection.Open();//connection state is open
+            SqlDataReader dataReader = cmd.ExecuteReader();//execute select statment
+            DataTable dataTable = new DataTable();
+             dataTable.Load(dataReader);
+            //DataTable--to store the records in table format
+            sqlConnection.Close(); //connection state is close
+            //if (result == 0)
+            //    return "dataTable is empty";
+                return dataTable;
+        }
+        public DataTable SelectEmployeeById(int EmpId)
+        {
+            SqlConnection sqlConnection = new SqlConnection(sqlConnectionStr);//connection establishment
+            string db = sqlConnection.Database;
+            SqlCommand cmd = new SqlCommand("select * from Employee where EmpId=" + EmpId, sqlConnection);
+            sqlConnection.Open();//connection state is open
+            SqlDataReader dataReader = cmd.ExecuteReader();//execute select statment
+            DataTable dataTable = new DataTable();
+            dataTable.Load(dataReader);
+            //DataTable, DataSet
+            sqlConnection.Close(); //connection state is close
+            return dataTable;
+        }
+       
+    }
+    
+
+}
---
+++ AdoDotNetHandsOn/AdoDotNetHandsOn/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace AdoDotNetHandsOn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeData employeeData = new EmployeeData();
+
+            employeeData.insertEmployee();             //insert employee records
+
+            employeeData.UpdateEmployee();           //update record
+
+            Console.WriteLine("Enter the EmpId to delete: ");   // delete record
+            int EmpId=Convert.ToInt32(Console.ReadLine());
+            employeeData.DeleteEmployee(EmpId);
+
+            DataTable dt = employeeData.SelectEmployees(); //select all the employee records
+            for (int i = 0; i < dt.Rows.Count; i++) //row iteration
+            {
+                for (int j = 0; j < dt.Columns.Count; j++) //column iteration
+               {
+                  Console.Write(dt.Rows[i][j] + "\t\t");
+               }
+           Console.WriteLine();
+           }
+
+            Console.WriteLine("Enter the EmpId to Select Employee: "); // select employee records by empid
+            int Empid = Convert.ToInt32(Console.ReadLine());
+            DataTable dt = employeeData.SelectEmployeeById(Empid);
+            for (int i = 0; i < dt.Rows.Count; i++) //row iteration
+            {
+                for (int j = 0; j < dt.Columns.Count; j++) //column iteration
+                {
+                    Console.Write(dt.Rows[i][j] + "\t\t");
+                }
+                //Console.WriteLine();
+            }
+
+            //account instance
+            AccountData accountData = new AccountData();
+
+            accountData.insertAccount();          //insert account records
+
+            accountData.updateAccount();            //update account records
+
+            Console.WriteLine("Enter the Account Id to delete: ");  //delete account records
+            int ActId = Convert.ToInt32(Console.ReadLine());
+            accountData.DeleteAccount(ActId);
+
+
+            DataTable dt = accountData.SelectAccounts();   //select all account records
+            for (int i = 0; i < dt.Rows.Count; i++) //row iteration
+            {
+                for (int j = 0; j < dt.Columns.Count; j++) //column iteration
+                {
+                    Console.Write(dt.Rows[i][j] + "\t\t");
+                }
+                Console.WriteLine();
+            }
+
+
+            Console.WriteLine("Enter the Account Id to Select Account: ");  //select account records by Act Id
+            int Actid = Convert.ToInt32(Console.ReadLine());
+            DataTable dt = accountData.SelectAccountById(Actid);
+            for (int i = 0; i < dt.Rows.Count; i++) //row iteration
+            {
+                for (int j = 0; j < dt.Columns.Count; j++) //column iteration
+                {
+                    Console.Write(dt.Rows[i][j] + "\t\t");
+                }
+                Console.WriteLine();
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
